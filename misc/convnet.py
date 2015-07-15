dea64296beedb5a9242b99a5f85273c61a36852f
Shen Xu <leoxushen@gmail.com>
2015-07-14 22:45:42 -0500

    update example of convnet

--- misc/convnet.py
+++ misc/convnet.py
@@ -35,12 +35,12 @@ def dropout(X, p= 0.):
     if p > 0:
         retain_prob = 1 - p
         X *= srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX)
-        x /= retain_prob
-        return X
+        X /= retain_prob
+    return X
 
 def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
     grads = T.grad(cost=cost, wrt=params)
-    udpates = []
+    updates = []
     for p, g in zip(params, grads):
         acc = theano.shared(p.get_value() * 0.)
         acc_new = rho * acc + (1 - rho) * g **2
@@ -66,16 +66,11 @@ def model(X, w, w2, w3, w4, w_o, p_drop_conv, p_drop_hidden):
     l3  = T.flatten(l3b, outdim=2)
     l3  = dropout(l3, p_drop_conv)
 
-    l4  = rectify(T.dot(l4, w4))
+    l4  = rectify(T.dot(l3, w4))
     l4 = dropout(l4, p_drop_hidden)
 
     pyx = softmax(T.dot(l4, w_o))
     return l1, l2, l3, l4, pyx
-
-
-
-
-
 
 
 def to_categorical(y, nb_classes=None):
@@ -116,7 +111,6 @@ def load_mnist(dataset):
 
     with gzip.open(dataset, 'rb') as handle:
         train_set, valid_set, test_set = cPickle.load(handle)
-    pdb.set_trace()   
     train_X, train_y = shared_data(train_set[0]),shared_data_int32(to_categorical(train_set[1]))
     valid_X, valid_y = shared_data(valid_set[0]),shared_data_int32(to_categorical(valid_set[1]))
     test_X, test_y   = shared_data(test_set[0]),shared_data_int32(to_categorical(test_set[1]))
@@ -127,4 +121,47 @@ def load_mnist(dataset):
 
 
 datasets = load_mnist('mnist.pkl.gz')
+(train_X, train_y) = datasets[0]
+(valid_X, valid_y) = datasets[1]
+(test_X, test_y) = datasets[2]
+
+train_X = train_X.reshape((-1,1,28,28))
+valid_X = valid_X.reshape((-1,1,28,28))
+test_X  = test_X.reshape((-1,1,28,28))
+
+X = T.ftensor4()
+y = T.fmatrix()
+
+w = init_weights((32,1,3,3))
+w2 = init_weights((64,32,3,3))
+w3 = init_weights((128,64,3,3))
+w4 = init_weights((128*3*3,625))
+w_o = init_weights((625,10))
+
+noise_l1, noise_l2, noise_l3, noise_l4, noise_py_x = model(X,w,w2,w3,w4,w_o,0.2,0.5)
+l1,l2,l3,l4,py_x = model(X,w,w2,w3,w4,w_o,0,0)
+y_x = T.argmax(py_x, axis=1)
+
+cost = T.mean(T.nnet.categorical_crossentropy(noise_py_x, y))
+params = [w,w2,w3,w4,w_o]
+updates = RMSprop(cost, params, lr=0.001)
+
+print "Compile Network"
+train = theano.function(
+        inputs = [X, y],
+        outputs = cost,
+        updates = updates, allow_input_downcast=True)
+
+predict = theano.function(
+        inputs = [X],
+        outputs=y_x,
+        allow_input_downcast=True)
+
+print "Start Training"
+num_iter = 0
+for i in xrange(100):
+    for start, end in zip(range(0,len(train_X),128),range(128,len(train_X),128)):
+        cost = train(train_X[start:end], train_y[start:end])
+        num_iter += 1
+        print 'Iteration %d, epoch %d: cost = %f'%(num_iter,i,cost)
 
