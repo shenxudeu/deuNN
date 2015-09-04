112ca7f13994719789e0baecf6586854b9982ffc
shenxudeu <leoxushen@gmail.com>
2015-09-04 15:56:18 -0500

    skip-grams implementation

--- deuNet/layers/word2vec.py
+++ deuNet/layers/word2vec.py
@@ -1,5 +1,5 @@
 """
-word2vec.py: Popular word2vec models
+word2vec.py: word2vec models
 
 References:
 "Efficient Estimation of Word Representations in Vector Space", 
@@ -13,5 +13,89 @@ import theano.tensor as T
 from .. import activations, initializations
 from .. layers.core import Layer
 from .. utils.theano_utils import sharedX
+
+
+class WordContext(Layer):
+    """
+    This is a very neat implementation of skip-grams model.
+    
+    Understanding 1:
+    Skip-grams model is MLP with 1 hidden layer without any
+    activations. It inputs a center word and predict its 
+    surrounding words. softmax and cross-entropy is applied.
+    |v|: vocab size
+    n: word representation size
+    C: surrounding window size
+    X: 1 x |v|, input one word with one-hot
+    Ww: |v| x n, inner world embedding matrix
+    Wc: |v| x n, outer world embedding matrix
+    h: 1 x n   = dot(X, Ww)
+    y: 1 x |v| = dot(h, Wc.T)
+    target can be think as |v| x 1 with surrounding words = 1
+    softmax and cross-entropy can be applied as lost function.
+    The training process is normal back-prop with SGD.
+
+    Understanding 2:
+    As described in [1] sector 2, we can think the dot product with one-hot as "Column Selection".
+    Then we can treat the NN (1 -> C) as multiple 1->1 simple weight products.
+    Then we can orginize the training data in one sentense (sequence) as mulitple samples with 1 input and 1 output.
+    We do the column selection Ww -> W: 1 x n
+                               Wc -> C: 1 x n
+    the matrix product of two vectors comes down to pair-wised product then sum.
+    Softmax is not needed to handle 1 output node, sigmoid can be used. The cost function also comes down to a MSE.
+
+    Understanding 3:
+    Another understanding of skip-grams model can be related to vector similarity. Our goal is use two matrices to 
+    present the whole vocab. Given a center word and one of the surrounding word, we can present them by two vectors.
+    The optimization goal is to maximize the similarity of those two vectors, since they accours close in the training
+    sentense. The similarity is calculated as vector dot product.
+
+    Note: [1] provides the detail of skip-gram model calculation, sub-sampling method, and negative-sampling method.
+
+    References: 
+    [1] Distributed Representations of Words and Phrases and their Compositionality
+        http://papers.nips.cc/paper/5021-distributed-representations-of-words-and-phrases-and-their-compositionality.pdf
+    [2] Efficient Estimation of Word Representations in Vector Space
+        http://arxiv.org/pdf/1301.3781.pdf
+    """
+    def __init__(self, input_dim, proj_dim=128,
+            init='uniform', activation = 'sigmoid', w_scale=1e-2):
+        super(WordContext, self).__init__()
+        self.input_dim = input_dim # |v|
+        self.proj_dim  = prof_dim # n
+        self.init = initializations.get(init)
+        self.activation = activations.get(activation)
+        self.w_scale = w_scale
+
+        self.input = T.imatrix()
+        self.W_w = self.init((input_dim, proj_dim), w_scale)
+        self.W_c = self.init((input_dim, proj_dim), w_scale)
+
+        self.params = [self.W_w, self.W_c]
+
+
+    def get_output(self, train=False):
+        X = self.get_input(train)
+        w = self.W_w[X[:,0]] # nb_sample x proj_dim, column selection
+        c = self.W_c[X[:,1]] # nb_sample x proj_dim, column selection
+
+        dot = T.sum(w * c, axis=1)
+        dot = T.reshape(dot, (X.shape[0], 1))
+        return self.activation(dot)
+
+    def get_config(self):
+        return {"name": self.__class__.__name__,
+                "input_dim": self.input_dim,
+                "proj_dim": self.proj_dim,
+                "init": self.init.__name__,
+                "activation": self.activation.__name__}
+
+
+
+
+
+
+
+
 
 
--- deuNet/preprocessing/text_sequence.py
+++ deuNet/preprocessing/text_sequence.py
@@ -1,8 +1,124 @@
 """
 Text Sequency processing
-includes word2vec sampling
+includes word2vec Negative Samplings
 """
 
 import numpy as np
 import random
+
+def pad_sequences(sequences, maxlen=None, dtype='int32', padding='pre', truncating='pre', value=0.):
+    """
+    Pad sequences to the same length, which is the length of longest one.
+    Sequences are presented by ints.
+    
+    Inputs:
+        - sequences: list, list of list of ints
+        - maxlen: int, max length of a sequence
+        - dtype: string, dtype of the sequence
+        - padding: string, "pre" or "post", pre or post padding
+        - truncating: string, "pre" or "pos", pre or post truncating
+        - value: int, padding value
+    Outpus:
+        - padded sequences, list of list of ints
+    """
+
+    lengths = [len(s) for s in sequences]
+
+    nb_samples = len(sequences)
+    if maxlen is None:
+        maxlen = np.max(lengths)
+
+    x = (np.ones((nb_samples, maxlen)) * value).astype(dtype)
+
+    for idx, s in enumerate(sequences):
+        if truncating == 'pre':
+            trunc = s[-maxlen:]
+        elif truncting == 'post':
+            trunc = s[:maxlen]
+        else:
+            raise ValueError("Truncating type '%s' not supported"% truncating)
+
+        if padding == 'post':
+            x[idx, :len(trunc)] = trunc
+        elif padding == 'pre':
+            x[idx, -len(trunc):] = trunc
+        else:
+            raise ValueError("Padding type '%s' not supported"% truncating)
+
+    return x
+
+
+def make_subsampling_table(size, sampling_factor = 1e-5):
+    """
+    The subsampling table is used to select training words.
+    Because in very large corpora, the most frequent words can
+    easily occur hundreds of millions of times, such words 
+    usually provide less information than the rare words.
+
+    Reference:
+    http://papers.nips.cc/paper/5021-distributed-representations-of-words-and-phrases-and-their-compositionality.pdf
+    Sector 2.3 - subsampling of frequent words
+    """
+    gamma = 0.577
+    rank = np.array(list(range(size)))
+    rank[0] = 1
+    inv_fq = rank * (np.log(rank) + gamma) + 0.5 - 1./(12.*rank)
+    f = sampling_factor * inv_fq
+    return np.minimum(1., f/np.sqrt(f))
+
+
+def skipgrams(sequence, vocabulary_size,
+        window_size=4, negative_samples=1., shuffle=True,
+        subsampling_table=None)
+    """
+    Generate training couples for skipgrams wrod2vec embedding
+    The generated dataset has two lists:
+        - couples: a list of list, sublist has length of 2, first is the center word, second is from surrounding word
+        - labels: a list, 1 means this is a positive sample, 0 means this is a negative sample
+    
+    Inputs:
+        - sequence: list of int, list of indices of words
+        - vocabulary_size: int, |v|
+        - window_size: int, half of the surrounding words length. The surrounding windows is [i-window_size, i+window_size]
+        - negative_samples: float, negative samples length (in percentage), 1. means the same size of positive samples
+        - shuffle: bool, True means shuffle the traning sample order
+        - subsampling_table: np.array, the array indicating subsampling prob density, generated from make_subsampling_table()
+    """
+    couples = []
+    labels = []
+
+    # loop each word in the sequence and construct positive training data
+    for i, wi in enumerate(sequence):
+        if not wi:
+            continue
+        if subsampling_table is not None:
+            if subsampling_table[wi] < random.random():
+                continue
+
+        window_start = max(0, 1-window_size)
+        window_end   = min(len(sequence), i + window_size +1)
+        for j in range(window_start, window_end):
+            if j != i:
+                wj = sequence[j]
+                if not wj:
+                    continue
+                couples.append([wi, wj])
+                labels.append(1)
+    
+    if negative_samples > 0:
+        nb_negative_samples = int(len(labels)*negative_samples)
+        words = [c[0] for c in couples] # all center words
+        random.shuffle(words)
+
+        couples += [[words[i%len(words)], random.randint(1, vocabulary_size-1)] for i in range(nb_negative_samples)]
+        labels  += [0] * nb_negative_samples
+    
+    if shuffle:
+        seed = random.randint(0, 10e6)
+        random.seed(seed)
+        random.shuffle(couples)
+        random.seed(seed)
+        random.shuffle(labels)
+
+    return couples, labels
 
