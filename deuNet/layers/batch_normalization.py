974a2ad93945a7a6cb0784c4455f60d82b41e452
shenxudeu <leoxushen@gmail.com>
2015-09-25 12:27:56 -0500

    local response normalization update

--- deuNet/layers/batch_normalization.py
+++ deuNet/layers/batch_normalization.py
@@ -82,4 +82,45 @@ class BatchNormalization(Layer):
                 "input_shape": self.input_shape,
                 "epsilon": self.epsilon}
         
+    
+class LRN2D(Layer):
+    """
+    Local Response Normalization
+    Adapted from pylearn2 for Alexnet use,
+    can be replaced by batch_normalization.
+    LRN is introduced by Alex as a part of AlexNet (section 3.3).
+    Reference: http://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf
+    The default value of alpha, k, beta, n are adapted from Alex in his paper.
+    """
+
+    def __init__(self, alpha=1e-4, k=2, beta=0.75, n=5):
+        if n % 2 == 0 :
+            raise NotImplementedError("LRN2D only works with odd n. n provided: " + str(n))
+        super(LRN2D, self).__init__()
+        self.alpha = alpha
+        self.k = k
+        self.beta = beta
+        self.n = n
+
+    def get_output(self, train):
+        X = self.get_inpu(train)
+        b, ch, r, c = X.shape
+        half_n = self.n // 2
+        input_sqrt = T.sqrt(X)
+        extra_channels = T.alloc(0., b, ch + 2 * half_n, r, c)
+        input_sqr = T.set_subtensor(extra_channels[:, half_n:half_n + ch, : , :], input_sqr)
+        scale = self.k
+        for i in range(self.n):
+            scale += self.alpha * input_sqr[:, i:i+ch, : , :]
+        scale = scale ** self.beta
+        return X/ scale
+
+    def get_config(self):
+        return {"name": self.__class__.__name__,
+                "alpha": self.alpha,
+                "k": self.k,
+                "beta": self.beta,
+                "n":self.n}
+
+
 
